9efa187284d0fca13283d3dcd437d0b9021d0143
vipul2511 <vshrimali709@gmail.com>
2019-11-22 19:57:49 +0530

    Blog Website Desgin

---
+++ sgs.js
@@ -0,0 +1,4 @@
+var x=document.getElementsByClassName("js-discover-person-card__action-btn full-width artdeco-button artdeco-button--2 artdeco-button--full artdeco-button--secondary ember-view");
+for(var i=0;i<10;i++){
+    x.click();
+}
